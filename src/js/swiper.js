2f7c44cad5db842a857fb95e0e2da839984757e5
Alsu Gatina <alsu8909@gmail.com>
2026-03-30 19:00:40 +0300

    Добавил новый блок slidemenu с переносом кнопки 6 на отдельную строку

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -56,3 +56,29 @@ button.addEventListener('click', () => {
     ? 'Скрыть'
     : 'Показать все'
 })
+const slider = document.querySelector('.slidemenu')
+let isDown = false
+let startX
+let scrollLeft
+
+slider.addEventListener('mousedown', (e) => {
+  isDown = true
+  slider.classList.add('active')
+  startX = e.pageX - slider.offsetLeft
+  scrollLeft = slider.scrollLeft
+})
+slider.addEventListener('mouseleave', () => {
+  isDown = false
+  slider.classList.remove('active')
+})
+slider.addEventListener('mouseup', () => {
+  isDown = false
+  slider.classList.remove('active')
+})
+slider.addEventListener('mousemove', (e) => {
+  if (!isDown) return
+  e.preventDefault()
+  const x = e.pageX - slider.offsetLeft
+  const walk = (x - startX) * 2 // скорость прокрутки
+  slider.scrollLeft = scrollLeft - walk
+})
